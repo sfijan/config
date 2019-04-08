64d43ad884af300846a3212b5df827ed199f3efa
sfijan <sven.j.fijan@gmail.com>
2019-04-08 18:08:17 +0200

    added git to prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,48 +3,73 @@ function vim
     nvim $argv
 end
 
+
 # Config git for dotfiles
 function config
     /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $argv
 end
 
+
 # Setting editor to nvim
 export VISUAL=nvim
 export EDITOR="$VISUAL"
 
-# Prompt
-function fish_prompt --description 'Write out the prompt'
-	set -l color_cwd
-    set -l suffix
-    switch "$USER"
-        case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
-            set suffix '#'
-        case '*'
-            set color_cwd $fish_color_cwd
-            set suffix '>'
-    end
 
-    echo -n -s (set_color $color_cwd) (prompt_pwd) (set_color normal) "$suffix "
+# Prompt
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+function fish_prompt
+    set last_status $status
+
+    set_color $fish_color_cwd
+    printf '%s' (prompt_pwd)
+    set_color normal
+
+    printf '%s ' (__fish_git_prompt)
+
+    set_color normal
 end
+
 
 # Remove greeting
 function fish_greeting
 end
 
+
 # Ruby
 status --is-interactive; and source (rbenv init -|psub)
+
 
 # Set default scrot location to ~/Pictures/screenshots/
 function screenshot
     scrot '%Y-%m-%d_%H:%M.png' -e 'mv --backup=t $f ~/Pictures/screenshots/' $argv
 end
 
+
 # set default xboxdrv config
 #function xboxdrv
 #    xboxdrv -c ~/.config/xboxdrv/config $argv
 #end
+
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
+
